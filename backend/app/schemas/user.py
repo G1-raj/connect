b7a72416f692684b81a09383c580f992e92a8b04
G1-raj <gullurajput18@gmail.com>
2026-01-18 22:50:27 +0530

    added schema for singup user and user profile creation

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,16 +1,37 @@
 from pydantic import EmailStr, BaseModel
+from typing import List
 
-class UserCreate(BaseModel):
-    pass
+class UserSignUp(BaseModel):
+    email: EmailStr
+    full_name: str
 
 class VerifyOtp(BaseModel):
-    pass
+    otp: str
 
 class PasswordCreate(BaseModel):
-    pass
+    password: str
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    full_name: str
+    date_of_birth: str
+    age: int
+    is_email_verified: bool
+    latitude: float
+    longitude: float
+    interests: List[str]
 
 class UserOut(BaseModel):
-    pass
+    id: int
+    email: EmailStr
+    full_name: str
+    date_of_birth: str
+    age: int
+    is_email_verified: bool
+    latitude: float
+    longitude: float
+    interestes: List[str]
 
 class UserCreateResponse(BaseModel):
     message: str
@@ -26,3 +47,6 @@ class LoginResponse(BaseModel):
     message: str
     data:UserOut
     token: TokenResponse
+
+class MessageResponse(BaseModel):
+    message: str
