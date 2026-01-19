8a15fe353027046db712ea6549a3fa22c6da1d9b
G1-raj <gullurajput18@gmail.com>
2026-01-19 16:24:32 +0530

    added the verify otp for email route

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -51,3 +51,49 @@ def signup(user: UserSignUp, db: Session = Depends(get_db)):
     return {
         "message": "Otp sent successfully, please verify your email"
     }
+
+
+@router.post("/verify-otp", response_model=MessageResponse, status_code=status.HTTP_200_OK)
+def verify_otp(otp: VerifyOtp, db: Session = Depends(get_db)):
+    
+    existing_user = (
+        db.query(models.User)
+        .filter(models.User.email == otp.email)
+        .first()
+    )
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User does not exist"
+        )
+
+    get_otp = (
+        db.query(models.EmailOtp)
+        .filter(models.EmailOtp.user_id == existing_user.id, models.EmailOtp.expires_at > datetime.now(timezone.utc))
+        .order_by(models.EmailOtp.created_at.desc())
+        .first()
+    )
+
+    if not get_otp:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Otp does not exist"
+        )
+    
+    if get_otp.attempts < 4:
+        check_otp = verify_hash(otp.otp, get_otp.otp_hash)
+
+        if check_otp:
+            db.delete(get_otp)
+            existing_user.is_email_verified = True
+        else:
+            get_otp.attempts += 1
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Otp expired or invalid"
+            )
+        
+    return {
+        "message": "Email verified successfully"
+    }
