7d1fa51b3e6c0f9b65a074144171cbc40263edcf
G1-raj <gullurajput18@gmail.com>
2026-01-23 23:20:10 +0530

    verifying current user

--- backend/app/routers/generate_feed.py
+++ backend/app/routers/generate_feed.py
@@ -2,3 +2,4 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.database.db import get_db
 from sqlalchemy.orm import Session
 from app.models import models
+from app.utils.dependencies import get_current_user
