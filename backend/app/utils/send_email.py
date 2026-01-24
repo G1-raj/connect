8e32e9d70c559c404381405ee04747668f3080ef
G1-raj <gullurajput18@gmail.com>
2026-01-24 23:31:57 +0530

    parameters

--- backend/app/utils/send_email.py
+++ backend/app/utils/send_email.py
@@ -2,5 +2,5 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def send_mail():
+def send_mail(to_email: str, subject: str, body: str):
     pass
