2bd2425859b7cc5bd661316bac5e0832b7232b38
G1-raj <gullurajput18@gmail.com>
2026-01-25 23:03:10 +0530

    changes

--- backend/app/utils/send_email.py
+++ backend/app/utils/send_email.py
@@ -1,6 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
+from email.mime.multipart import MIMEMultipart
 
 # def send_mail(to_email: str, subject: str, body: str):
 #     msg = MIMEMultipart()
