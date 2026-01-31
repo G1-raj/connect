0859b76a275534b3617bb4e9227e9a85cf0eab0a
G1-raj <gullurajput18@gmail.com>
2026-01-31 21:19:21 +0530

    added the smtp environments

--- backend/app/utils/send_email.py
+++ backend/app/utils/send_email.py
@@ -1,12 +1,12 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+# import smtplib
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
 
-def send_mail(to_email: str, subject: str, body: str):
-    msg = MIMEMultipart()
+# def send_mail(to_email: str, subject: str, body: str):
+#     msg = MIMEMultipart()
 
-    msg["From"] = EMAIL_FROM
-    msg["To"] = to_email
-    msg["Subject"] = subject
+#     msg["From"] = EMAIL_FROM
+#     msg["To"] = to_email
+#     msg["Subject"] = subject
 
-    msg.attach(MIMEText(body, 'html'))
+#     msg.attach(MIMEText(body, 'html'))
