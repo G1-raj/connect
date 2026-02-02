6b795c1b0ffad2b99da247298684c2a84bd4cc4e
G1-raj <gullurajput18@gmail.com>
2026-02-02 23:09:02 +0530

    Banner placement

--- frontend/connect/lib/app.dart
+++ frontend/connect/lib/app.dart
@@ -9,7 +9,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-      home: LoginScreen(),
+      home: AuthNavigationScreen(),
+    );
+  }
+}
+
+class AuthNavigationScreen extends StatelessWidget {
+  AuthNavigationScreen({super.key});
+
+  final AssetImage _banner = AssetImage("lib/assets/connect_banner.jpg");
+
+  @override
+  Widget build(BuildContext context) {
+
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            width: screenWidth,
+            height: screenHeight,
+            child: Image(
+              image: _banner,
+              fit: BoxFit.fitHeight,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
