b0ee17bff23019a3ac442826030de46b0598f95f
G1-raj <gullurajput18@gmail.com>
2026-02-02 23:29:44 +0530

    Auth navigtor screen finish

--- frontend/connect/lib/app.dart
+++ frontend/connect/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:connect/core/theme/theme.dart';
 import 'package:connect/features/auth/presentation/login_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -35,9 +36,71 @@ class AuthNavigationScreen extends StatelessWidget {
               image: _banner,
               fit: BoxFit.fitHeight,
             ),
+          ),
+
+          Container(
+            width: screenWidth,
+            height: screenHeight,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(0, 0, 0, 0.6)
+            ),
+          ),
+
+          //Logo
+          Positioned(
+            left: screenWidth * 0.3,
+            top: screenHeight * 0.1,
+            child: Container(
+              width: screenWidth * 0.4,
+              height: screenWidth * 0.35,
+              decoration: BoxDecoration(
+                  color: AppTheme.themeRed,
+                  shape: BoxShape.circle
+              ),
+            ),
+          ),
+
+          //Slogan
+          Positioned(
+            top: screenHeight * 0.5,
+            left: screenWidth * 0.02,
+            child: sloganSection(screenHeight, screenWidth)
           )
         ],
       ),
+    );
+  }
+
+  Widget sloganSection(double screenHeight, double screenWidth) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        RichText(
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: screenWidth * 0.07,
+              fontWeight: FontWeight.w600,
+              height: 1.0
+            ),
+            children: [
+              TextSpan(
+                text: "Find your ",
+                style: TextStyle(color: AppTheme.whiteBackground),
+              ),
+              TextSpan(
+                text: "Partner",
+                style: TextStyle(color: AppTheme.themeRed),
+              ),
+            ],
+          ),
+        ),
+
+
+        Text("Let's find your real life partner and enjoy your good life from now!", style: TextStyle(
+          color: AppTheme.whiteBackground,
+          fontSize: screenWidth * 0.025
+        ),)
+      ],
     );
   }
 }
