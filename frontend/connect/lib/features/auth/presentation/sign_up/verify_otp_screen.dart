3fd418894ea0c1658919ef0bb8f6a3ee28d7a35a
G1-raj <gullurajput18@gmail.com>
2026-02-03 17:47:35 +0530

    verify otp screen initialization

--- frontend/connect/lib/features/auth/presentation/sign_up/verify_otp_screen.dart
+++ frontend/connect/lib/features/auth/presentation/sign_up/verify_otp_screen.dart
@@ -1,3 +1,5 @@
+import 'package:connect/core/theme/theme.dart';
+import 'package:connect/core/widgets/app_button/app_button.dart';
 import 'package:flutter/material.dart';
 
 class VerifyOtpScreen extends StatelessWidget {
@@ -5,10 +7,64 @@ class VerifyOtpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+
     return Scaffold(
-      body: Center(
-        child: Text("Verify otp screen"),
+
+      backgroundColor: AppTheme.whiteBackground,
+
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: screenWidth * 0.05, top: screenHeight * 0.04),
+                  child: Text("Verify Otp", style: TextStyle(
+                    color: AppTheme.blackBackground,
+                    fontWeight: FontWeight.w800,
+                    fontSize: screenWidth * 0.06,
+                    letterSpacing: 0.5
+                  ),),
+                ),
+              ),
+
+              Spacer(),
+
+              AppButton(
+                width: screenWidth * 0.95,
+                height: screenHeight * 0.06,
+                text: "Verify",
+                buttonColor: AppTheme.themeRed,
+                textColor: AppTheme.whiteBackground,
+                fontSize: screenWidth * 0.04,
+              ),
+        
+              SizedBox(
+                height: screenHeight * 0.02,
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class OtpField extends StatefulWidget {
+  const OtpField({super.key});
+
+  @override
+  State<OtpField> createState() => _OtpFieldState();
+}
+
+class _OtpFieldState extends State<OtpField> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
