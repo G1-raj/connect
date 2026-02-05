2e047fe2322977165b1be41d48432a7f3eeba8d7
G1-raj <gullurajput18@gmail.com>
2026-02-05 23:38:51 +0530

    profile images page

---
+++ frontend/connect/lib/features/auth/presentation/sign_up/profile_images.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileImages extends StatelessWidget {
+  const ProfileImages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
