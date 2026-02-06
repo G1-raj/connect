ada6f465c0cf358d6966bc73c9e564248de14c07
G1-raj <gullurajput18@gmail.com>
2026-02-06 22:43:18 +0530

    Router initiated for sexuality screen

--- frontend/connect/lib/core/routing/router.dart
+++ frontend/connect/lib/core/routing/router.dart
@@ -4,6 +4,7 @@ import 'package:connect/features/auth/presentation/sign_up/gender_screen.dart';
 import 'package:connect/features/auth/presentation/sign_up/interests_screen.dart';
 import 'package:connect/features/auth/presentation/sign_up/password_screen.dart';
 import 'package:connect/features/auth/presentation/sign_up/profile_images.dart';
+import 'package:connect/features/auth/presentation/sign_up/sexuality_screen.dart';
 import 'package:connect/features/auth/presentation/sign_up/signup_screen.dart';
 import 'package:connect/features/auth/presentation/sign_up/verify_otp_screen.dart';
 import 'package:go_router/go_router.dart';
@@ -61,6 +62,11 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: "/date-of-birth",
       builder: (context, state) => DateOfBirthScreen(),
+    ),
+
+    GoRoute(
+      path: "/sexuality",
+      builder: (context, state) => SexualityScreen(),
     )
   ]
 );
