6006f1b41cb2d34f0305eff6954435c498ced70b
G1-raj <gullurajput18@gmail.com>
2026-02-15 10:14:48 +0530

    Implemented the respositories for signup

--- frontend/connect/lib/features/auth/data/auth_repository_impl.dart
+++ frontend/connect/lib/features/auth/data/auth_repository_impl.dart
@@ -1,5 +1,13 @@
+import 'dart:ffi';
+
 import 'package:connect/features/auth/data/models/login/login_request.dart';
 import 'package:connect/features/auth/data/models/login/login_response.dart';
+import 'package:connect/features/auth/data/models/signup/request/create_profile_request.dart';
+import 'package:connect/features/auth/data/models/signup/request/otp_request.dart';
+import 'package:connect/features/auth/data/models/signup/request/password_request.dart';
+import 'package:connect/features/auth/data/models/signup/request/signup_request.dart';
+import 'package:connect/features/auth/data/models/signup/response/message_response.dart';
+import 'package:connect/features/auth/data/models/signup/response/otp_response.dart';
 import 'package:connect/features/auth/data/services/auth_api_service.dart';
 import 'package:connect/features/auth/domain/auth_repository.dart';
 
@@ -12,6 +20,58 @@ class AuthRepositoryImpl implements AuthRepository {
   Future<LoginResponse> login(String email, String password) async {
     final res = await api.login(
       LoginRequest(email: email, password: password)
+    );
+
+    return res;
+  }
+
+  @override
+  Future<MessageResponse> signup(String email, String fullName) async {
+    final res = await api.signup(
+      SignupRequest(email: email, fullName: fullName)
+    );
+
+    return res;
+  }
+
+  @override
+  Future<OtpResponse> verifyOtp(String email, String otp) async {
+    final res = await api.verifyOtp(
+      OtpRequest(email: email, otp: otp)
+    );
+
+    return res;
+  }
+
+  @override
+  Future<MessageResponse> createPassword(String password) async {
+    final res = await api.createPassword(
+      PasswordRequest(password: password)
+    );
+
+    return res;
+  }
+
+  @override
+  Future<MessageResponse> createProfile(
+    String gender, 
+    String description, 
+    String sexuality, 
+    DateTime dateOfBirth, 
+    double longitude, 
+    double latitude, 
+    List<String> interests
+  ) async  {
+    final res = await api.createProfile(
+      CreateProfileRequest(
+        gender: gender, 
+        description: description, 
+        sexuality: sexuality, 
+        dateOfBirth: dateOfBirth, 
+        longitude: longitude, 
+        latitude: latitude, 
+        interests: interests
+      )
     );
 
     return res;
--- frontend/connect/lib/features/auth/domain/auth_repository.dart
+++ frontend/connect/lib/features/auth/domain/auth_repository.dart
@@ -1,5 +1,24 @@
+import 'dart:ffi';
+
 import 'package:connect/features/auth/data/models/login/login_response.dart';
+import 'package:connect/features/auth/data/models/signup/response/message_response.dart';
+import 'package:connect/features/auth/data/models/signup/response/otp_response.dart';
 
 abstract class AuthRepository {
+    //login
     Future<LoginResponse> login(String email, String password);
+
+    //signup
+    Future<MessageResponse> signup(String email, String fullName);
+    Future<OtpResponse> verifyOtp(String email, String otp);
+    Future<MessageResponse> createPassword(String password);
+    Future<MessageResponse> createProfile(
+      String gender, 
+      String description, 
+      String sexuality, 
+      DateTime dateOfBirth, 
+      double longitude, 
+      double latitude, 
+      List<String> interests
+    );
 }
