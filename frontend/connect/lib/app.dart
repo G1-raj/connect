6894f3567703c481b511553a33f8bf9b8328103c
G1-raj <gullurajput18@gmail.com>
2026-02-17 07:43:52 +0530

    added loader to the signup screen

--- frontend/connect/lib/app.dart
+++ frontend/connect/lib/app.dart
@@ -1,23 +1,16 @@
 import 'package:connect/core/routing/router.dart';
-import 'package:connect/core/widgets/loader/loader.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) {  
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Loader(),
-      ),
+      theme: ThemeData.light(),
+      routerConfig: router,
     );
-    // return MaterialApp.router(
-    //   debugShowCheckedModeBanner: false,
-    //   theme: ThemeData.light(),
-    //   routerConfig: router,
-    // );
   }
 }
 
--- frontend/connect/lib/features/auth/presentation/sign_up/signup_screen.dart
+++ frontend/connect/lib/features/auth/presentation/sign_up/signup_screen.dart
@@ -25,196 +25,213 @@ class SignupScreen extends ConsumerWidget {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
 
+    ref.listen(signupControllerProvider, (prev, next) {
+      if(next.loading == true) {
+        showDialog(
+          context: context, 
+          barrierDismissible: false,
+          barrierColor: AppTheme.loaderBackground,
+          builder: (_) {
+            return Center(
+              child: Image(
+                image: AssetImage("lib/assets/loader.png"),
+              ),
+            );
+          });
+      }
+
+      if(prev?.loading == true && next.loading == false) {
+        context.pop();
+      }
+
+
+      if (next.error != null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.red,
+            content: Text(next.error!)
+          ),
+        );
+      }
+    });
+
     return Scaffold(
       backgroundColor: AppTheme.whiteBackground,
       body: SafeArea(
-        child: Stack(
-          children: [
-            Form(
-              key: _formKey,
-              child: Center(
-                child: Column(
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.04,
+                ),
+                //Logo
+                Hero(
+                  tag: "logo",
+                  child: Image(
+                    image: AssetImage("lib/assets/logo.png"),
+                    width: 180,
+                  ),
+                ),
+        
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+          
+                Text("Signup", style: TextStyle(
+                  fontSize: screenWidth * 0.06,
+                  fontWeight: FontWeight.w600
+                ),),
+        
+                SizedBox(
+                  height: screenHeight * 0.05,
+                ),
+        
+                InputField(
+                  hintText: "Email", 
+                  prefixIcon: Icon(Icons.email), 
+                  textController: _emailController,
+                  width: screenWidth * 0.85,
+                  height: screenHeight * 0.07,
+                  validator: (value) {
+                    if(value == null || value.isEmpty) {
+                      return "Please provide the email";
+                    }
+        
+                    if(!value.contains("@")) {
+                      return "Please provide the valid gmail address";
+                    }
+        
+                    return null;
+                  },
+                ),
+        
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+        
+                InputField(
+                  hintText: "Full Name", 
+                  prefixIcon: Icon(Icons.person), 
+                  textController: _nameController,
+                  width: screenWidth * 0.85,
+                  height: screenHeight * 0.07,
+                  validator: (value) {
+                    if(value == null || value.isEmpty) {
+                      return "Please provide your full name";
+                    }
+        
+                    return null;
+                  },
+                ),
+        
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+        
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SizedBox(
-                      height: screenHeight * 0.04,
-                    ),
-                    //Logo
-                    Hero(
-                      tag: "logo",
-                      child: Image(
-                        image: AssetImage("lib/assets/logo.png"),
-                        width: 180,
-                      ),
-                    ),
-            
-                    SizedBox(
-                      height: screenHeight * 0.01,
-                    ),
-              
-                    Text("Signup", style: TextStyle(
-                      fontSize: screenWidth * 0.06,
-                      fontWeight: FontWeight.w600
-                    ),),
-            
-                    SizedBox(
-                      height: screenHeight * 0.05,
-                    ),
-            
-                    InputField(
-                      hintText: "Email", 
-                      prefixIcon: Icon(Icons.email), 
-                      textController: _emailController,
-                      width: screenWidth * 0.85,
-                      height: screenHeight * 0.07,
-                      validator: (value) {
-                        if(value == null || value.isEmpty) {
-                          return "Please provide the email";
-                        }
-            
-                        if(!value.contains("@")) {
-                          return "Please provide the valid gmail address";
-                        }
-            
-                        return null;
-                      },
-                    ),
-            
-                    SizedBox(
-                      height: screenHeight * 0.02,
-                    ),
-            
-                    InputField(
-                      hintText: "Full Name", 
-                      prefixIcon: Icon(Icons.person), 
-                      textController: _nameController,
-                      width: screenWidth * 0.85,
-                      height: screenHeight * 0.07,
-                      validator: (value) {
-                        if(value == null || value.isEmpty) {
-                          return "Please provide your full name";
-                        }
-            
-                        return null;
-                      },
-                    ),
-            
-                    SizedBox(
-                      height: screenHeight * 0.01,
-                    ),
-            
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Already have an account?"),
-                        TextButton(onPressed: () {
-                          context.pop();
-                        }, child: Text("Login", style: TextStyle(
-                          color: AppTheme.themeRed,
-                          fontWeight: FontWeight.bold
-                        ),))
-                      ],
-                    ),
-            
-                    Spacer(),
-            
-                    SizedBox(
-                      width: screenWidth * 0.85,
-                      child: RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          style: TextStyle(
-                            color: AppTheme.blackBackground,
-                            fontWeight: FontWeight.w500,
-                            wordSpacing: 0.6
-                          ),
-                          children: [
-                            TextSpan(
-                              text: "By continuing, you agree to our ",
-                            ),
-                      
-                            TextSpan(
-                              text: "User Agreement",
-                              style: TextStyle(
-                                color: AppTheme.themeRed,
-                                fontWeight: FontWeight.w700,
-                                decoration: TextDecoration.underline
-                              )
-                            ),
-                      
-                            TextSpan(
-                              text: " and acknowledge that you understand the "
-                            ),
-                      
-                            TextSpan(
-                              text: "Privacy Policy",
-                              style: TextStyle(
-                                color: AppTheme.themeRed,
-                                fontWeight: FontWeight.w700,
-                                decoration: TextDecoration.underline
-                              )
-                            ),
-                      
-                      
-                          ]
-                        ),
-                      ),
-                    ),
-            
-                    SizedBox(
-                      height: screenHeight * 0.03,
-                    ),
-              
-                    AppButton(
-                      width: screenWidth * 0.95,
-                      height: screenHeight * 0.06,
-                      text: "Signup",
-                      buttonColor: AppTheme.themeRed,
-                      textColor: AppTheme.whiteBackground,
-                      fontSize: screenWidth * 0.04,
-                      onPress: () {
-                        if(_formKey.currentState!.validate()) {
-                          _formKey.currentState!.save();
-            
-                          showDialog(
-                            context: context,
-                            barrierDismissible: true,
-                            builder: (context) => PopUpDialog(
-                              title: "Verify Email",
-                              body: "Confirm your email address",
-                              email: _emailController.text,
-                              onConfirm: () async {
-                                final isSuccess = await signupCtrl.signup(_emailController.text, _nameController.text);
-                                
-            
-                                if(isSuccess && context.mounted) {
-                                  context.push("/verify-otp");    
-                                }
-
-                                // context.push("/verify-otp", extra: _emailController.text); 
-                              },
-                            ),
-                          );
-                        }
-                      },
-                    ),
-              
-                    SizedBox(
-                      height: screenHeight * 0.02,
-                    )
+                    Text("Already have an account?"),
+                    TextButton(onPressed: () {
+                      context.pop();
+                    }, child: Text("Login", style: TextStyle(
+                      color: AppTheme.themeRed,
+                      fontWeight: FontWeight.bold
+                    ),))
                   ],
                 ),
-              ),
+        
+                Spacer(),
+        
+                SizedBox(
+                  width: screenWidth * 0.85,
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: AppTheme.blackBackground,
+                        fontWeight: FontWeight.w500,
+                        wordSpacing: 0.6
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "By continuing, you agree to our ",
+                        ),
+                  
+                        TextSpan(
+                          text: "User Agreement",
+                          style: TextStyle(
+                            color: AppTheme.themeRed,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.underline
+                          )
+                        ),
+                  
+                        TextSpan(
+                          text: " and acknowledge that you understand the "
+                        ),
+                  
+                        TextSpan(
+                          text: "Privacy Policy",
+                          style: TextStyle(
+                            color: AppTheme.themeRed,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.underline
+                          )
+                        ),
+                  
+                  
+                      ]
+                    ),
+                  ),
+                ),
+        
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+          
+                AppButton(
+                  width: screenWidth * 0.95,
+                  height: screenHeight * 0.06,
+                  text: "Signup",
+                  buttonColor: AppTheme.themeRed,
+                  textColor: AppTheme.whiteBackground,
+                  fontSize: screenWidth * 0.04,
+                  onPress: () {
+                    if(_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+        
+                      showDialog(
+                        context: context,
+                        barrierDismissible: true,
+                        builder: (context) => PopUpDialog(
+                          title: "Verify Email",
+                          body: "Confirm your email address",
+                          email: _emailController.text,
+                          onConfirm: () async {
+                            final isSuccess = await signupCtrl.signup(_emailController.text, _nameController.text);
+                            
+        
+                            if(isSuccess && context.mounted) {
+                              context.push("/verify-otp");    
+                            }
+        
+                            // context.push("/verify-otp", extra: _emailController.text); 
+                          },
+                        ),
+                      );
+                    }
+                  },
+                ),
+          
+                SizedBox(
+                  height: screenHeight * 0.02,
+                )
+              ],
             ),
-
-            if(signupState.loading)
-              Center(
-                child: CircularProgressIndicator(),
-              ),
-            if(signupState.error != null)
-              Center(
-                child: Text(signupState.error!),
-              ), 
-          ],
+          ),
         ),
       )
     );
