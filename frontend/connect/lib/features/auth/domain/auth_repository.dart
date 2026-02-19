6d76230d7df213b7a1c5a5e89c20c8ce4ac40f83
G1-raj <gullurajput18@gmail.com>
2026-02-19 23:17:41 +0530

    Added the upload pictures to auth repository

--- frontend/connect/lib/features/auth/domain/auth_repository.dart
+++ frontend/connect/lib/features/auth/domain/auth_repository.dart
@@ -1,23 +1,32 @@
+import 'dart:io';
+
 import 'package:connect/features/auth/data/models/login/login_response.dart';
 import 'package:connect/features/auth/data/models/signup/response/message_response.dart';
 import 'package:connect/features/auth/data/models/signup/response/otp_response.dart';
 
 abstract class AuthRepository {
-    //login
-    Future<LoginResponse> login(String email, String password);
+  //login
+  Future<LoginResponse> login(String email, String password);
 
-    //signup
-    Future<MessageResponse> signup(String email, String fullName);
-    Future<OtpResponse> verifyOtp(String email, String otp);
-    Future<MessageResponse> createPassword(String password, String onboardingToken);
-    Future<MessageResponse> createProfile(
-      String gender, 
-      String description, 
-      String sexuality, 
-      DateTime dateOfBirth, 
-      double longitude, 
-      double latitude, 
-      List<String> interests,
-      String onboardingToken
-    );
+  //signup
+  Future<MessageResponse> signup(String email, String fullName);
+  Future<OtpResponse> verifyOtp(String email, String otp);
+  Future<MessageResponse> createPassword(
+    String password,
+    String onboardingToken,
+  );
+  Future<MessageResponse> createProfile(
+    String gender,
+    String description,
+    String sexuality,
+    DateTime dateOfBirth,
+    double longitude,
+    double latitude,
+    List<String> interests,
+    String onboardingToken,
+  );
+  Future<MessageResponse> uploadPictures(
+    List<File> images,
+    String onboardingToken,
+  );
 }
