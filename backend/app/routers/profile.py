b22fca1aa4a3101a1798996ccac624370f0325a4
G1-raj <gullurajput18@gmail.com>
2026-02-20 19:27:21 +0530

    Create the basic profile structure

--- backend/app/routers/profile.py
+++ backend/app/routers/profile.py
@@ -5,3 +5,7 @@ from app.database.db import get_db
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/user", tags=["user", "profile"])
+
+@router.get("/profile", status_code=status.HTTP_200_OK)
+def get_user_profile(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    pass
