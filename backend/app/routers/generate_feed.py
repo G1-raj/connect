8a19c4a78a54ccdffc44941ab2a66abfa87e0f36
G1-raj <gullurajput18@gmail.com>
2026-02-20 23:50:40 +0530

    Initialized the generate feed

--- backend/app/routers/generate_feed.py
+++ backend/app/routers/generate_feed.py
@@ -3,3 +3,7 @@ from app.database.db import get_db
 from sqlalchemy.orm import Session
 from app.models import models
 from app.utils.dependencies import get_current_user
+
+router = APIRouter(prefix="/profile", tags=["feed"])
+
+@router.get("/feed", status_code=status.HTTP_200_OK)
