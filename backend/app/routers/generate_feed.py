9c30d8b25d96b45b71e69de5b711203092a7ccdb
G1-raj <gullurajput18@gmail.com>
2026-02-20 23:55:27 +0530

    Added parameters on function

--- backend/app/routers/generate_feed.py
+++ backend/app/routers/generate_feed.py
@@ -7,3 +7,5 @@ from app.utils.dependencies import get_current_user
 router = APIRouter(prefix="/profile", tags=["feed"])
 
 @router.get("/feed", status_code=status.HTTP_200_OK)
+def generate_feed(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    pass
