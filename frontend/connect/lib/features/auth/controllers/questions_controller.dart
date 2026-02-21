1ab9cd0b686d56148daedf00fa3a942d45daf251
G1-raj <gullurajput18@gmail.com>
2026-02-21 23:39:42 +0530

    Created the controller for questions api route

--- frontend/connect/lib/features/auth/controllers/questions_controller.dart
+++ frontend/connect/lib/features/auth/controllers/questions_controller.dart
@@ -1,3 +1,7 @@
+import 'package:connect/shared/providers/auth_repository_provider.dart';
+import 'package:connect/shared/providers/storage_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 class QuestionState {
   final bool loading;
   final String? error;
@@ -11,5 +15,53 @@ class QuestionState {
       error: error,
       success: success ?? this.success,
     );
+  }
+}
+
+class QuestionsController extends Notifier<QuestionState> {
+  @override
+  QuestionState build() {
+    return const QuestionState();
+  }
+
+  Future<void> answerQuestions(
+    bool alcohol,
+    bool smoke,
+    bool kids,
+    bool pets,
+    bool exercise,
+  ) async {
+    state = state.copyWith(loading: true, error: null, success: false);
+
+    try {
+      final repo = ref.read(authRepositoryProvider);
+      final storage = ref.read(storageProvider);
+
+      final onboardingToken = await storage.read(key: "onboarding_token");
+
+      if (onboardingToken == null) throw Exception("No onboarding token");
+
+      final res = await repo.answerQuestions(
+        alcohol,
+        smoke,
+        kids,
+        pets,
+        exercise,
+        onboardingToken,
+      );
+
+      if (res.message!.isNotEmpty) {
+        state = state.copyWith(loading: false, error: null, success: true);
+        return;
+      }
+
+      state = state.copyWith(
+        loading: false,
+        error: "Failed to answer questions",
+        success: false,
+      );
+    } catch (e) {
+      throw Exception("Failed to answer questions");
+    }
   }
 }
