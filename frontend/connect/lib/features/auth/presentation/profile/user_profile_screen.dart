e70945b0d88190bc753ed669b59847efbbfab44f
G1-raj <gullurajput18@gmail.com>
2026-02-24 23:12:07 +0530

    Added the mediaquery for responsiveness

--- frontend/connect/lib/features/auth/presentation/profile/user_profile_screen.dart
+++ frontend/connect/lib/features/auth/presentation/profile/user_profile_screen.dart
@@ -6,6 +6,9 @@ class UserProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: AppTheme.whiteBackground,
 
